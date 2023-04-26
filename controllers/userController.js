b68106c79c841999a298e0a6ca1b3fe605302960
vilena81 <shadyanvilena@gmail.com>
2023-04-26 18:18:11 +0400

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,56 +50,83 @@ exports.verified = async (req, res) => {
 
 }
 
-exports.register = async (req, res) => {
-    const { error } = regSchema.validate(req.body);
-    if (error) {
-        return res.status(400).json({ error: error.details[0].message });
-    }
-    const { role, email, userName, firstName, lastName, password } = req.body
-    const emailRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])[^\s@]+@[^\s@]+\.[^\s@]{2,}$/
-    if (!emailRegex.test(email)) {
-        return res.status(400).json({ error: "Invalid email format" })
-    }
-    console.log(Users_schema)
-    await Users_schema.findOne({ where: { email: email } })
-        .then((user) => {
-            if (user) {
-                return res.status(400).json({ error: "Email already exists" })
-            }
-            const hashed_password = CryptoJS.SHA256(password).toString()
-            Users_schema.create({
-                role, userName, firstName, lastName, email,
-                password: hashed_password, is_Verified: 0
-            })
-                .then((data) => {
-                    let token = generateAccessToken(email, 0)
-                    send_mail(email, token)
-                    res.status(201).json({message: "User created"})
-                }).catch((err) => {
-                    res.status(500).json({ error: err.message })
-                })
+// exports.register = async (req, res) => {
+//     const { error } = regSchema.validate(req.body);
+//     if (error) {
+//         return res.status(400).json({ error: error.details[0].message });
+//     }
+//     const { role, email, userName, firstName, lastName, password } = req.body
+//     const emailRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])[^\s@]+@[^\s@]+\.[^\s@]{2,}$/
+//     if (!emailRegex.test(email)) {
+//         return res.status(400).json({ error: "Invalid email format" })
+//     }
+//     console.log(Users_schema)
+//     await Users_schema.findOne({ where: { email: email } })
+//         .then((user) => {
+//             if (user) {
+//                 return res.status(400).json({ error: "Email already exists" })
+//             }
+//             const hashed_password = CryptoJS.SHA256(password).toString()
+//             Users_schema.create({
+//                 role, userName, firstName, lastName, email,
+//                 password: hashed_password, is_Verified: 0
+//             })
+//                 .then((data) => {
+//                     let token = generateAccessToken(email, 0)
+//                     send_mail(email, token)
+//                     res.status(201).json({ message: "User created" })
+//                 }).catch((err) => {
+//                     res.status(500).json({ error: err.message })
+//                 })
+//         })
+// }
+
+exports.register = (req,res)=>{
+    const {role, email, userName, firstName, lastName ,password, is_Verified} = req.body;
+    const {error} = regSchema.validate(req.body);
+              if(error){
+                  return res.status(400).json({error: error.details[0].message});
+              }
+    // const emailRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])[^\s@]+@[^\s@]+\.[^\s@]{2,}$/ ;
+    // if(!emailRegex.test(email)){
+    //   return res.status(400).json({error: 'Invalid email format'})
+    // }
+    Users_schema.findOne({where: {email: email}})
+    .then((user)=>{
+      if(user){
+        return res.status(400).json({error:"Email already exists"})
+      }
+      const hashed_password = CryptoJS.SHA256(password).toString()
+      Users_schema.create({ role, userName, firstName ,lastName ,email, password:hashed_password,  is_Verified:0})
+        .then((data)=>{
+            let token = generateAccessToken(email, 0)
+            send_mail(email, token)
+            res.status(201).json({message : "User created"})
+        }).catch((err)=>{
+            res.status(500).json({error:err.message})
         })
-}
-
-exports.login = async (req, res) => {
-    const { error } = logSchema.validate(req.body);
-    if (error) {
-        return res.status(400).json({ error: error.details[0].message });
+      })
     }
-    const { email, password } = req.body
-    const hashed_password = CryptoJS.SHA256(password).toString()
-    await Users_schema.findOne({ where: { email, password: hashed_password } })
-        .then((data) => {
-            let token = generateAccessToken(data.role)
-            if (data.email === email && data.password === hashed_password) {
-                res.send(JSON.stringify({ status: "Logged in", jwt: token }))
-            } else {
-                res.send(JSON.stringify({ status: "Wrong credentials" }));
-            }
-        })
+
+exports.login = (req, res)=>{
+    const {email, password} = req.body;
+    const {error} = logSchema.validate(req.body);
+    console.log(email)
+    if(error){
+        return res.status(400).json({error: error.details[0].message});
+    }
+    const hashed_password = CryptoJS.SHA256(password).toString();
+    Users_schema.findOne({where: {email:email}})
+    .then((data)=> {
+      console.log(data.password, hashed_password)
+        if(data && data.password === hashed_password && data.is_Verified == 1){
+            let token = generateAccessToken(email, data.role);
+            res.send(JSON.stringify({status: "Logged in", jwt: token, role: data.role, is_Verified: data.is_Verified}))
+        }else{
+            res.send(JSON.stringify({status: "Not logged in"}));
+        }
+    }) 
 }
-
-
 
 exports.getAllUsers = (req, res) => {
     Users_schema.findAll()
--- function/validation.js
+++ function/validation.js
@@ -7,7 +7,8 @@ exports.regSchema = Joi.object({
     userName: Joi.string().min(3).max(30).required(),
     firstName: Joi.string().min(3).max(30).required(),
     lastName: Joi.string().min(3).max(30).required(),
-    email: Joi.string().email().required(),
+    // email: Joi.string().email().required(),
+    email: Joi.string().email({ tlds: { allow: false } }).required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-z0-9]{3,30}$')).required(),
 })
 
